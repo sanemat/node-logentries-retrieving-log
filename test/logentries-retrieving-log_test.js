8b4d070a67b6b9aaf23087cb6172f5592b667a0a
sanemat <o.gata.ken@gmail.com>
2014-12-18 00:03:06 +0900

    Add interface

--- test/logentries-retrieving-log_test.js
+++ test/logentries-retrieving-log_test.js
@@ -2,6 +2,7 @@
 
 var LogentriesRetrievingLog = require('../');
 var assert = require('power-assert');
+var nock = require('nock');
 
 describe('logentriesRetrievingLog', function () {
   var retriever;
@@ -11,8 +12,14 @@ describe('logentriesRetrievingLog', function () {
     done();
   });
 
-  it('should be awesome', function () {
-    assert(logentriesRetrievingLog() == 'awesome');
+  it('should retrieve logs', function (done) {
+    // https://pull.logentries.com/__YOUR_KEY__/__LOG_ADDR__/?limet=10
+    nock('https://pull.logentries.com')
+      .get('/__YOUR_KEY__/__LOG_ADDR__/?limet=10')
+      .replyWithFile(200, __dirname + '/logentries-com-retrieved-logs.txt');
+    retriever.get({limit: 10}, function(err, data){
+      assert.equal(data.lines.length, 10);
+      done();
+    });
   });
-
 });
