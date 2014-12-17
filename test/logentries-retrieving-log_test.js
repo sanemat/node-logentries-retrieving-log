4e9b22ed905cb5df5c3e4de8b2763d182f950def
sanemat <o.gata.ken@gmail.com>
2014-12-17 23:51:27 +0900

    Set up retriever

--- test/logentries-retrieving-log_test.js
+++ test/logentries-retrieving-log_test.js
@@ -4,6 +4,12 @@ var LogentriesRetrievingLog = require('../');
 var assert = require('power-assert');
 
 describe('logentriesRetrievingLog', function () {
+  var retriever;
+
+  before(function(done){
+    retriever = LogentriesRetrievingLog({ accountKey: '__YOUR_KEY__', logAddr: '__LOG_ADDR__' });
+    done();
+  });
 
   it('should be awesome', function () {
     assert(logentriesRetrievingLog() == 'awesome');
